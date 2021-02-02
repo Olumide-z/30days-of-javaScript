13420368e23a6fda8838bf59569543a3270f8473
Olumide <ilesanmiolumide10@gmail.com>
2021-02-02 13:30:46 +0100

    introduction

---
+++ 01-Introduction/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof "Olumide");
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof null);
---
+++ 01-Introduction/variable.js
@@ -0,0 +1,46 @@
+let firstName = "Olumide" //string
+let secondName //undefined
+const lastName = true //boolean
+let middleName = null //null
+
+console.log(firstName, secondName, lastName, middleName);
+
+//variables without assigning values
+
+let address;
+let school;
+let home;
+let church;
+
+console.log(address, school, home, church);
+
+//variables with assigning values
+
+let address1 = "Lagos state";
+let school1 = "FUTA";
+let home1 = "Ikeja";
+let church1 = "RCCG";
+
+console.log(address1, school1, home1, church1);
+
+// Variable that store multiple values
+
+let firstName2 = "Olumide",
+    lastName2 = "Ilesanmi",
+    maritalStatus2 = "Single",
+    country2 = "Nigeria",
+    age2 = 22
+
+    console.log(firstName2, lastName2, maritalStatus2, country2, age2);
+
+    let firstName3 = "Olumide", lastName3 = "Ilesanmi", maritalStatus3 = "Single", country3 = "Nigeria", age3 = 22
+
+    console.log(firstName3, lastName3, maritalStatus3, country3, age3);
+
+//age variables
+
+let myAge = 22;
+let yourAge = 30;
+
+console.log("i am" + " "+ myAge + "years old");
+console.log("you are" + " "+ yourAge + "years old");
