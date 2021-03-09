bda919ab963846121aebd68e2d22bb2e64ed2d70
Olumide <ilesanmiolumide10@gmail.com>
2021-03-09 13:15:52 +0100

    writting-clean-codes

---
+++ 20-writing-clean-code/explanation.js
@@ -0,0 +1,30 @@
+//JavaScript Coding Conventions
+   /* In this challenge also we used the general JavaScript coding writing conventions and guides. 
+    Coding conventions are style guidelines for programming which are developed by an individual, a team or a company.*/
+
+//Coding conventions helps:
+
+    //to write clean code
+    //to improve code readability
+    //to improve code re-useability and maintainability
+
+//Coding conventions includes
+
+    //Naming and declaration rules for variables
+    //Naming and declaration rules for functions
+    //Rules for the use of white space, indentation, and comments
+    //Programming practices and principles
+
+//Conventions use in 30DaysOfJavaScript
+    //In this challenge we follow the regular JavaScript convention but I added also my preference of writing.
+
+    //We used camelCase for variables and functions.
+    //All variable names start with a letter.
+    //We chose to use const for constants, arrays, objects and functions. In stead of double quote, we chose to use single quote or backtick. Single quote is becoming trendy.
+    //We also removed semicolons from our code but it is a matter of personal preference.
+    //Space around arithmetic operators, assignment operators and after comma
+    //Arrow function instead of function declaration
+    //Explicit return instead of implicit return if the function is one liner
+    //No trailing comma in the last value of an object
+    //We prefer this +=, -=, *= /=, **= instead of the longer version
+    //When we use console.log() it is good to print with a tag string to identify from where the console is coming
